2a6c3ca49072554589a9c7073b311042f65c1bc3
Adam Rhazi <78358394+2027111@users.noreply.github.com>
2025-04-01 00:01:24 -0400

    index

---
+++ send_email.php
@@ -0,0 +1,25 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = htmlspecialchars($_POST["name"]);
+    $email = htmlspecialchars($_POST["email"]);
+    $message = htmlspecialchars($_POST["message"]);
+
+    $to = "adamrhazi@hotmail.ca"; 
+    $subject = "New Contact Form Message from $name";
+    $headers = "From: $email\r\n";
+    $headers .= "Reply-To: $email\r\n";
+    $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
+
+    $body = "Name: $name\n";
+    $body .= "Email: $email\n";
+    $body .= "Message:\n$message\n";
+
+    if (mail($to, $subject, $body, $headers)) {
+        echo "Your message has been sent successfully.";
+    } else {
+        echo "There was an error sending your message.";
+    }
+} else {
+    echo "Invalid request.";
+}
+?>
